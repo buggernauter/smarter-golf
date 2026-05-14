be5b5c4f3692e673221a0a7b81650362f93ab69a
buggernauter <khazar.mehraban@gmail.com>
2026-05-14 11:22:20 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,12 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: [
+        "favicon.ico",
+        "favicon-96x96.png",
         "favicon.svg",
         "apple-touch-icon.png",
-        "pwa-192.png",
-        "pwa-512.png",
-        "maskable-512.png",
+        "web-app-manifest-192x192.png",
+        "web-app-manifest-512x512.png",
       ],
       manifest: {
         id: "/",
@@ -30,17 +31,13 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "/pwa-192.png",
+            src: "/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "maskable",
           },
           {
-            src: "/pwa-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/maskable-512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
